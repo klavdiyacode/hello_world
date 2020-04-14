960d4f197a273e82737f1f1c6232fe02b046dbe2
klavdiyacode <akmatsadykov.abdel@gmail.com>
2020-04-14 11:27:20 +0600

    somechanges

---
+++ hello_world/src/test/java/com/lol/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lol.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
